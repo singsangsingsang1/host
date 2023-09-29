b15a10ad81880c104c33321d6c9b63ba9da5c736
singsangsingsang1 <113302450+singsangsingsang1@users.noreply.github.com>
2023-09-28 17:40:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,8 @@ ngrok.get_tunnels()
 
 from pyngrok import ngrok
 
-
+WEBCAMX = 100
+WEBCAMY = 100
 
 log = logging.getLogger('werkzeug')
 log.disabled = True
@@ -160,10 +161,10 @@ def TakeScreenshots(Data):
 
 
 
-Frame = np.zeros((10, 10, 3), dtype=np.uint8)
+Frame = np.zeros((WEBCAMX, WEBCAMy, 3), dtype=np.uint8)
 def Webcam():
     global Frame
-    with pyvirtualcam.Camera(width=10, height=10, fps=20) as cam:
+    with pyvirtualcam.Camera(width=WEBCAMX, height=WEBCAMY, fps=20) as cam:
         while True:
             cam.send(Frame)
             cam.sleep_until_next_frame()
@@ -178,7 +179,7 @@ def RebuildCamera(data):
         for i in range(AM):
             Image.append((R, G, B))
         
-    return np.array(Image).reshape((10, 10, 3)).astype(np.uint8)
+    return np.array(Image).reshape((WEBCAMX, WEBCAMY, 3)).astype(np.uint8)
 
 def WebcamDaemon():
     WebcamThread = threading.Thread(target=Webcam)
