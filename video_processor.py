5c0ff811927a00ba02d13ca63d16fff7de9b3bab
singsangsingsang1 <113302450+singsangsingsang1@users.noreply.github.com>
2023-12-01 21:34:03 -0800

    Update video_processor.py

--- video_processor.py
+++ video_processor.py
@@ -115,7 +115,7 @@ def inputs():
 
         json_payload = fast_json.dumps({"X": X, "Y": Y, "Fps": fps}).encode('utf-8')
         payload_length = struct.pack('I', len(json_payload))
-        payload = os.urandom(10) + payload_length + json_payload + processed
+	payload = os.urandom(10) + payload_length + json_payload + processed
 	response = payload
 
 
